57a54cca44c5e05428acb3232df63fcd3211bd95
Omer Faruk Kilic <omeerfkiliic@gmail.com>
2023-12-10 23:46:23 +0300

    Done Part 3 Except Challenge

--- 1-Create and Run Simple C# Console Applications/ChallengeProject/ChallengeProject/Starter/Program.cs
+++ 1-Create and Run Simple C# Console Applications/ChallengeProject/ChallengeProject/Starter/Program.cs
@@ -1,23 +1,3 @@
-/* 
-This C# console application is designed to:
-- Use arrays to store student names and assignment scores.
-- Use a `foreach` statement to iterate through the student names as an outer program loop.
-- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
-- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
-- Use an algorithm within the outer loop to calculate the average exam score for each student.
-- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
-- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
-    - detects extra credit assignments based on the number of elements in the student's scores array.
-    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
-- use the following report format to report student grades: 
-
-    Student         Grade
-
-    Sophia:         92.2    A-
-    Andrew:         89.6    B+
-    Emma:           85.6    B
-    Logan:          91.2    A-
-*/
 int examCount = 5;
 int extraPointModifier = 10;
 
@@ -36,14 +16,7 @@ string letterGrade = "";
 Console.Clear();
 Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
-/*
-The outer foreach loop is used to:
-- iterate through student names 
-- assign a student's grades to the studentScores array
-- sum assignment scores (inner foreach loop)
-- calculate numeric and letter grade
-- write the score report information
-*/
+
 foreach (string name in studentNames)
 {
     string currentStudent = name;
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,23 +1,102 @@
 ﻿/*
-   This code reverses a message, counts the number of times 
-   a particular character appears, then prints the results
-   to the console window.
- */
+string? inputFromUser;
+int numberInInput;
+bool validNumber = false;
 
-string originalMessage = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine("Enter an integer value between 5 and 10");
 
-char[] messageGroup = originalMessage.ToCharArray();
-Array.Reverse(messageGroup);
-
-int letterCount = 0;
-
-foreach (char i in messageGroup) 
+do
 {
-     if (i == 'o') 
-        letterCount++; 
+   inputFromUser = Console.ReadLine();
+   validNumber = int.TryParse(inputFromUser, out numberInInput);
+   
+   if(!validNumber)
+   {
+      Console.WriteLine("Sorry, you entered an invalid number, please try again");
+      continue;
+   }
+
+   else if(numberInInput < 5 || numberInInput > 10)
+   {
+      Console.WriteLine($"You entered {numberInInput}. Please enter a number between 5 and 10.");
+      validNumber = false;
+   }
+   
+
+} while (inputFromUser == null || !validNumber);
+
+Console.WriteLine($"Your input value {(numberInInput)} has been accepted.");
+*/
+/*
+using System.Reflection.Metadata;
+
+string? inputFromUser;
+string roleToMatch;
+bool isRoleValid = false;
+string[] roles = new string[3] { "Administrator", "Manager", "User" };
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+   inputFromUser = Console.ReadLine();
+   roleToMatch = inputFromUser.Trim().ToLower();
+
+   switch (roleToMatch)
+   {
+      case "administrator":
+      case "manager":
+      case "user":
+         Console.WriteLine($"Your input value ({inputFromUser.Trim()}) has been accepted.");
+         isRoleValid = true;
+         break;
+
+      default:
+         Console.WriteLine($"The role name that you entered, '{inputFromUser.Trim()}' is not valid. Enter your role name (Administrator, Manager, or User)");
+         isRoleValid = false;
+         break;
+
+   }
+
+} while (inputFromUser == null || !isRoleValid);
+*/
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+   myString = myStrings[i];
+   periodLocation = myString.IndexOf(".");
+
+   string mySentence;
+
+   // extract sentences from each string and display them one at a time
+   while (periodLocation != -1)
+   {
+
+      // first sentence is the string value to the left of the period location
+      mySentence = myString.Remove(periodLocation);
+
+      // the remainder of myString is the string value to the right of the location
+      myString = myString.Substring(periodLocation + 1);
+
+      // remove any leading white-space from myString
+      myString = myString.TrimStart();
+
+      // update the comma location and increment the counter
+      periodLocation = myString.IndexOf(".");
+
+      Console.WriteLine(mySentence);
+   }
+
+   mySentence = myString.Trim();
+   Console.WriteLine(mySentence);
 }
 
-string reverseMessage = new String(messageGroup);
 
-Console.WriteLine(reverseMessage);
-Console.WriteLine($"'o' appears {letterCount} times.");
+
+
